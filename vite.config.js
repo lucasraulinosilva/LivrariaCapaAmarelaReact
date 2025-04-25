14ddc1a162c836619be5e6ece517389b390a6997
lucasraulino <91342114+lucasraulinosilva@users.noreply.github.com>
2025-04-25 15:37:10 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT || 10000, 
+  },
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
 })
